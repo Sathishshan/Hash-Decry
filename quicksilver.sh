a5d56b8480e9e53d9891ac31c7e1d2cc1f55369d
Sathish Kumar <23581693+Sathishshan@users.noreply.github.com>
2019-05-04 09:02:26 +0530

    removed argument check and added asking for user_input

--- quicksilver.sh
+++ quicksilver.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
 
-# Argument Check
-
-if [ $# -ne 1 ]; then
-        echo "Need one hash crack it."
-        echo "USAGE:
-        $0 hash_value"
-        exit
-fi
-
 # Hash Check
-hash=$1
+echo -n  "Enter the hash value: "
+read hash
 count=$(echo -n $hash | wc -m)
 
 # Working Area
 if [ "$count" -eq 32 ]; then
-        echo "its a MD5 hash"
+        echo "It's a MD5 hash"
         hashfor=$(curl -s "http://hashtoolkit.com/reverse-hash/?hash=${hash}" | sed -n 219p | cut -d '>' -f 4 | cut -d "<" -f 1)
         if [ -z $hashfor ]; then
                 echo "But, no hash crack found!"
@@ -24,7 +16,7 @@ if [ "$count" -eq 32 ]; then
                 exit
         fi
 elif [ "$count" -eq 40 ]; then
-        echo "its a SHA1 hash"
+        echo "It's a SHA1 hash"
         hashfor=$(curl -s "http://hashtoolkit.com/reverse-hash/?hash=${hash}" | sed -n 219p | cut -d '>' -f 4 | cut -d "<" -f 1)
         if [ -z $hashfor ]; then
                 echo "But, no hash crack found!"
@@ -33,7 +25,7 @@ elif [ "$count" -eq 40 ]; then
                 exit
         fi
 elif [ "$count" -eq 64 ]; then
-        echo "its a SHA-256 hash"
+        echo "It's a SHA-256 hash"
         hashfor=$(curl -s "http://hashtoolkit.com/reverse-hash/?hash=${hash}" | sed -n 219p | cut -d '>' -f 4 | cut -d "<" -f 1)
         if [ -z $hashfor ]; then
                 echo "But, no hash crack found!"
@@ -42,7 +34,7 @@ elif [ "$count" -eq 64 ]; then
                 exit
         fi
 elif [ "$count" -eq 96 ]; then
-        echo "its a SHA-384 hash"
+        echo "It's a SHA-384 hash"
         hashfor=$(curl -s "http://hashtoolkit.com/reverse-hash/?hash=${hash}" | sed -n 219p | cut -d '>' -f 4 | cut -d "<" -f 1)
         if [ -z $hashfor ]; then
                 echo "But, no hash crack found!"
@@ -51,7 +43,7 @@ elif [ "$count" -eq 96 ]; then
                 exit
         fi
 elif [ "$count" -eq 128 ]; then
-        echo "its a SHA-512 hash"
+        echo "It's a SHA-512 hash"
         hashfor=$(curl -s "http://hashtoolkit.com/reverse-hash/?hash=${hash}" | sed -n 219p | cut -d '>' -f 4 | cut -d "<" -f 1)
         if [ -z $hashfor ]; then
                 echo "But, no hash crack found!"
