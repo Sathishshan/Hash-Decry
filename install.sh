d6d1eec1af1bf605c2a08e84992dcaa51c6e7dfb
Sathish Kumar <23581693+Sathishshan@users.noreply.github.com>
2019-05-05 17:43:41 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,3 @@
+sudo cp ./quicksilver.sh /usr/bin/quicksilver  
+chmod u+x /usr/bin/quicksilver
+echo "Installation Done!, Now you can use the script from anywhere with the Operating System by calling its name."
